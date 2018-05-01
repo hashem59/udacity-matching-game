6db9a1d4ee4c6bbdd6cb3d653750561abc5d1195
Hashem Ramdan <34623487+hashem59@users.noreply.github.com>
2018-05-01 13:17:50 +0200

    update cardsClickable function

--- js/app.js
+++ js/app.js
@@ -86,6 +86,9 @@ for (let x in cards) {
         openCrds.push(cards[x]);
         //console.log(openCrds);
         if (openCrds.length === 2) {
+	    for ( let j = 0; j < cards.length; j++) {
+	    cards[j].style.pointerEvents = "none";
+            }
             if (openCrds[0].classList[2] === openCrds[1].classList[2]) {
                 openCrds[0].classList.add("match", "rubberBand");
                 openCrds[1].classList.add("match", "rubberBand");
